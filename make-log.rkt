00822cdb4bed93bb5200882812198d31720351f8
janhye <janhye@icloud.com>
2019-10-15 14:57:00 +0800

    added .gitignore

--- make-log.rkt
+++ make-log.rkt
@@ -15,24 +15,29 @@
   (define (start-logger)
     (let ([receiver (make-log-receiver my-logger 'debug)]
           [date-str (format-date (current-date))])
+      (define (start-loop out)
+        (let loop ()
+          (match (sync receiver)
+            [(vector level message value topic)
+             (when out
+               (fprintf out "~a - ~a - ~a\n" level message value)
+               (flush-output out))
+             (printf "~a - ~a - ~a\n" level message value)
+             (flush-output)])
+          (loop)))
       (set! logger-thread
             (thread
              (lambda ()
-               (when log-dir
-                 (when (not (directory-exists? log-dir))
-                   (make-directory log-dir)))
-               (call-with-output-file 
-                   (build-path log-dir (string-append (substring date-str 0 10) ".log")) #:exists 'append
-                 (lambda (out)
-                   (let loop ()
-                     (match (sync receiver)
-                       [(vector level message value topic)
-                        (when log-dir
-                          (fprintf out "~a - ~a - ~a\n" level message value)
-                          (flush-output out))
-                        (printf "~a - ~a - ~a\n" level message value)
-                        (flush-output)])
-                     (loop)))))))))
+               (if log-dir
+                   (begin
+                     (when (not (directory-exists? log-dir))
+                       (make-directory log-dir))
+                     (call-with-output-file 
+                         (build-path log-dir (string-append (substring date-str 0 10) ".log")) #:exists 'append
+                       (lambda (out)
+                         (start-loop out))
+                       ))
+                   (start-loop #f)))))))
   
   (define (restart-logger)
     (kill-thread logger-thread)
